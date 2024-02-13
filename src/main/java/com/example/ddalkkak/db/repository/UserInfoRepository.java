d54bdc2bff9dcf8d76a7ef11f114bd372d8cf94d
Chan <ieejo716@naver.com>
2024-02-13 13:23:28 +0900

    project merge

--- src/main/java/com/example/ddalkkak/db/repository/UserInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.ddalkkak.db.repository;
-
-import com.example.ddalkkak.db.model.UserInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
-    UserInfo findBySlackId(String slackId);
-}
