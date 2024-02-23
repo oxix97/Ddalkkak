f24b061e38da1c2f04b1fde8d119335e7598aa8e
Chan <ieejo716@naver.com>
2024-02-23 12:28:46 +0900

    Chore : View 관련 작업

---
+++ DdalkkakAndroid/app/src/main/java/com/example/ddalkkak_android/util/View.Ext.kt
@@ -0,0 +1,14 @@
+package com.example.ddalkkak_android.util
+
+import android.content.Context
+import android.widget.Toast
+
+private var toast: Toast? = null
+
+fun Context.shortToast(text: String) {
+    if (toast == null)
+        toast = Toast.makeText(this, text, Toast.LENGTH_SHORT)
+    else
+        toast!!.setText(text)
+    toast?.show()
+}
