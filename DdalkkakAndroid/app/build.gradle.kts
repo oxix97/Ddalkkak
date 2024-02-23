37a311c7cb2c9031cf5ae49b8aeb5ea63262182c
Chan <ieejo716@naver.com>
2024-02-23 12:26:41 +0900

    Chore : room 의존성 주입

--- DdalkkakAndroid/app/build.gradle.kts
+++ DdalkkakAndroid/app/build.gradle.kts
@@ -1,4 +1,3 @@
-import org.jetbrains.kotlin.storage.CacheResetOnProcessCanceled.enabled
 import java.io.FileInputStream
 import java.util.Properties
 
@@ -60,10 +59,10 @@ android {
 }
 
 dependencies {
-
-    //DataStore
-    implementation ("androidx.datastore:datastore-preferences:1.0.0")
-    implementation("androidx.datastore:datastore-preferences-core:1.0.0")
+    implementation ("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    kapt ("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
 
     // SavedState
     implementation ("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.7.0")
